f9a2f284e8817c5fbb8457f9b04a8e3c75e77cf6
Katha Villanueva <katha.villanueva@police.vic.gov.au>
2024-10-28 15:12:24 +1100

    feat: add web app

--- Azure Web App Test/obj/Debug/net8.0/Azure Web App Test.AssemblyInfo.cs
+++ Azure Web App Test/obj/Debug/net8.0/Azure Web App Test.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Azure Web App Test")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19cfa5e733202a7d7ad3f89bfbfd2cfb4a95458d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Azure Web App Test")]
